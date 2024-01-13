42876ad52212df933e8a50fdbcec67be5dc2ad2a
ooutaksaluk <156273867+ooutaksaluk@users.noreply.github.com>
2024-01-13 20:02:02 +0700

    9 button1

--- MoleSmasher.py
+++ MoleSmasher.py
@@ -1,23 +1,38 @@
-import random
 import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.clock import Clock
+import random
 
-class MyBoxLayoutApp(App):
+class MyButtonGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyButtonGrid, self).__init__(**kwargs)
+        self.cols = 3
+        self.buttons = []
+        self.create_buttons()
+        Clock.schedule_interval(self.create_random_button, 1.5)  #สร้างปุ่มทุก 1.5 วินาที
+
+    def create_buttons(self):
+        for i in range(9):
+            button = Button(text='', on_press=self.on_button_press)
+            self.buttons.append(button)
+            self.add_widget(button)
+# สุ่มตำแหน่งของปุ่ม
+    def create_random_button(self, dt):
+        available_buttons = [b for b in self.buttons if b.text == '']
+        if available_buttons:
+            random_button = random.choice(available_buttons)
+            random_button.text = 'Mole'
+#ลบปุ่มที่ถูกคลิก
+    def on_button_press(self, instance):
+        instance.text = ''
+
+
+class MyApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        return MyButtonGrid()
 
-        button1 = Button(text='Button 1')
-        button2 = Button(text='Button 2')
-        layout.add_widget(button1)
-        layout.add_widget(button2)
-
-        return layout
 
 if __name__ == '__main__':
-    MyBoxLayoutApp().run()
+    MyApp().run()
